0e01006e8e0dac7d1f190685f2ad80cc61be9e5f
FernandoRuiz87 <fruizjimenenez@gmail.com>
2025-10-05 20:41:25 -0600

    style: Improve code formatting in login DTOs for better readability

--- src/auth/dto/login-student.dto.ts
+++ src/auth/dto/login-student.dto.ts
@@ -1,13 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
-export class LoginStudentDto{
-  @ApiProperty({ example: 202312345, description: 'ID único del estudiante (expediente)' })
+export class LoginStudentDto {
+  @ApiProperty({
+    example: 202312345,
+    description: 'ID único del estudiante (expediente)',
+  })
   @IsNumber()
   @IsNotEmpty()
   studentId: number;
 
-  @ApiProperty({ example: 'ASFKLJAHFLKSA897Y431248ASDJKASDH', description: 'Contraseña del estudiante' })
+  @ApiProperty({
+    example: 'ASFKLJAHFLKSA897Y431248ASDJKASDH',
+    description: 'Contraseña del estudiante',
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
